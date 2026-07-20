77c606945d81d30c25563be8b767a2c379c9209e
Vishnu <vishnu29.krr@gmail.com>
2026-07-20 09:26:12 +0530

    Connected Railway backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
     try {
       const response = await axios.post(
-        "http://claimsense-ai-production-8a50.up.railway.app/upload",
+        "https://claimsense-ai-production-3240.up.railway.app/upload",
         formData,
       );
 
@@ -58,7 +58,7 @@ function App() {
 
     try {
       const response = await axios.post(
-        "http://claimsense-ai-production-8a50.up.railway.app/ask",
+        "https://claimsense-ai-production-3240.up.railway.app/ask",
         {
           question,
         },
